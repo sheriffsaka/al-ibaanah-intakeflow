efdd2e57c969c7241c1231ffe43dc9accb1e574f
Sheriffsaka <sheriffdeenalade@gmail.com>
2026-02-03 21:02:21 +0100

    Added new notification and checkin update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  optimizeDeps: {
+    include: ['qrcode'], // ✅ Add this line
   },
   build: {
     outDir: 'dist',
