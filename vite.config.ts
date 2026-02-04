6c0cfdefd5bba730180653c5dd409e3e2e220d85
Sheriffsaka <sheriffdeenalade@gmail.com>
2026-02-04 14:07:31 +0100

    Added multi language and gender slots

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  optimizeDeps: {
+    include: ['qrcode', 'html5-qrcode'], // ✅ Add this
   },
   build: {
     outDir: 'dist',
